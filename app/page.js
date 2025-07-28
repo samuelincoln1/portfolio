9a05997e98dc6bdcf54d31b5b5817dc6a94a2f9b
samuel <samuelincoln1@gmail.com>
2025-07-27 23:03:40 -0300

    fix: loading

--- app/page.js
+++ app/page.js
@@ -6,25 +6,61 @@ import Articles from "./components/Articles";
 import Navbar from "./components/util/Navbar";
 import ParticlesBackground from "./components/effects/ParticlesBackground";
 import Footer from "./components/Footer";
+
 export default function Index() {
-  const [bgLoaded, setBgLoaded] = useState(false);
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
 
   useEffect(() => {
-    const img = new window.Image();
-    img.src = "/images/clouds.jpg";
-    img.onload = () => setBgLoaded(true);
+      const imagesToPreload = [
+      "/images/clouds.jpg",
+      "/images/terraform.jpg",
+      "/images/lambda.png", 
+      "/images/cicd.png",
+      "/images/On-premise-to-Cloud.png",
+      "/images/serverless.png",
+      "/images/cicd2.png",
+      "/images/profile.jpg",
+      "/images/transition.png",
+      "/images/mono.png",
+      "/images/cicdarticle.png",
+      "/images/aws-certified-solutions-architect-associate.png"
+    ];
+
+    let loadedCount = 0;
+    let loadPromises = [];
+
+    imagesToPreload.forEach((src) => {
+      const promise = new Promise((resolve, reject) => {
+        const img = new window.Image();
+        img.src = src;
+        img.onload = () => {
+          loadedCount++;
+          setLoadingProgress((loadedCount / imagesToPreload.length) * 100);
+          resolve();
+        };
+        img.onerror = () => {
+           loadedCount++;
+          resolve();
+        };
+      });
+      loadPromises.push(promise);
+    });
+
+    Promise.all(loadPromises).then(() => {
+      setAllImagesLoaded(true);
+    });
   }, []);
 
   return (
     <div className="relative">
-      {!bgLoaded && (
+      {!allImagesLoaded && (
         <div className="fixed inset-0 z-[5000] flex items-center justify-center bg-black">
           <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
         </div>
       )}
       <div
         className="bg-[url('/images/clouds.jpg')] bg-cover bg-center bg-no-repeat min-h-screen bg-sky-300"
-        style={{ visibility: bgLoaded ? "visible" : "hidden" }}
+        style={{ visibility: allImagesLoaded ? "visible" : "hidden" }}
       >
         <div className="absolute inset-0 bg-black/45" id="home"></div>
         <Navbar />
